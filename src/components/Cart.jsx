c310758ec43908499f53273af279644b67422044
shubhansh0311 <shubhanshagrawal0311@gmail.com>
2024-09-22 20:49:34 +0530

    updated20.8.48

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,22 +3,22 @@ import { Link } from "react-router-dom";
 const Cart = ({ AddItem, order,removeItem, cartQuantity }) => {
     // const TotalCost=order.items.reduce((total,item)=>total+item.price*item.quantity,0);
 
-    return ( 
-        <div class="container mb-5 " style={{background:"white",marginTop:"10px"}}>
-            <div class="d-flex row flex-column m-2 BG-LIGHT justify-content-start flex-md-row bg-light  align-items-start  ">
+    return (
+        <div class="container mb-5" style={{background:'white',marginTop:"10px"}}>
+            <div class="d-flex row flex-column m-2 justify-content-start flex-md-row align-items-start  ">
 
-                <h3 className="mt-12" >My Cart: {!order.items.length ? 'no items within the cart' : null}</h3>
+                <h3>My Cart: {!order.items.length ? 'no items within the cart' : null}</h3>
                 <div class="col-md-7 mx-md-2 p-0 ">
                     
                     {AddItem.map(myItem =>
-                        <div class="cart-item p-3 " style={{marginBottom:"10px"}}>
+                        <div class="cart-item p-3   ">
                             <div class=" d-flex flex-row ">
                                 <img class="img-fluid col-2 py-2" src={`images/${myItem.image}.jpg`} alt="" />
                                 <div class="col-6">
                                     <h5>{myItem.brand}</h5>
                                     <h6>{myItem.name}</h6>
                                     <h6>Quantity: {myItem.quantity}</h6>
-                                    <p>Price : ₹{myItem.price}</p>
+                                    <p>Price : ${myItem.price}</p>
                                     <div class="d-flex">
                                         <input type="radio" class="bg-danger" name="color" id="red" />RED
                                         <input type="radio" class="bg-primary" name="color" id="blue" />BLUE
@@ -34,7 +34,7 @@ const Cart = ({ AddItem, order,removeItem, cartQuantity }) => {
                                     </select>
 
                                 </div>
-                                <div onClick={()=>removeItem(myItem)}
+                                <div onClick={()=>removeItem(AddItem)}
                                     data-bs-target="#removemyModal"><i class="bi bi-x-circle-fill"></i>
 
                                 </div>
@@ -56,21 +56,21 @@ const Cart = ({ AddItem, order,removeItem, cartQuantity }) => {
                     </div>
                     <div class="d-flex py-2 justify-content-between">
                         <span class="billing-item">Item Cost</span>
-                        <span class="billing-cost">₹{order.totalCost}</span>
+                        <span class="billing-cost">${order.totalCost}</span>
                     </div>
                     <div class="d-flex py-2 justify-content-between">
                         <span class="billing-item">Ship</span>
-                        <span class="billing-cost">₹{order.shipping}</span>
+                        <span class="billing-cost">${order.shipping}</span>
                     </div>
                     <div class="d-flex py-2 justify-content-between">
                         <span class="billing-item">Discount</span>
-                        <span class="billing-cost">{order.discount}%</span>
+                        <span class="billing-cost">${order.discount}</span>
                     </div>
                     <div class="d-flex py-2 justify-content-between">
                         <span class="billing-item">Total</span>
-                        <span class="billing-cost">₹{order.totalCost + order.shipping - order.discount * order.totalCost / 100}</span>
+                        <span class="billing-cost">${order.totalCost + order.shipping - order.discount * order.totalCost / 100}</span>
                     </div>
-                    <Link to="/checkout.html" class="button btn py-2 addToCartBtn btn-primary">Buy-now</Link>
+                    <Link to="/checkout.html" class="button btn py-2 btn-primary">Buy-now</Link>
                 </div> : null}
 
             </div>
